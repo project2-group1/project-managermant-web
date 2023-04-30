1771534de87a59bff63bfa372b3ae27cb02d1533
mxngocqb <87193435+mxngocqb@users.noreply.github.com>
2023-04-30 16:43:41 +0700

    Ngoc

--- backend/database.sql
+++ backend/database.sql
@@ -9,7 +9,7 @@ SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
 -- Tạo database
-CREATE DATABASE PROJECT_II
+CREATE DATABASE PROJECT_II;
 --
 -- Cấu trúc bảng `teacher`
 --
@@ -39,17 +39,33 @@ CREATE TABLE `student`(
 --
 CREATE TABLE `group`(
 	`group_id` int(8) NOT NULL
-)
+);
 --
--- Cấu trúc bảng `metting`
+-- Cấu trúc bảng `meeting`
 --
-CREATE TABLE `metting`(
+CREATE TABLE `meeting`(
     `group_id` int(8) NOT NULL,
     `teacher_id` int(8) NOT NULL,
-	`metting_id` int(8) NOT NULL,
+	`meeting_id` int(8) NOT NULL,
     `date` DATE DEFAULT NULL,
     `reportdeadline` DATE DEFAULT NULL,
     `note` varchar(200) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
-    `next_metting_id` int(8) NOT NULL,
+    `next_meeting_id` int(8) NOT NULL,
   	`report` varchar(200) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_vietnamese_ci;
+
+-- Tao key cho bang `teacher`
+ALTER TABLE `teacher`
+	ADD PRIMARY KEY (`teacher_id`);
+-- Tao key cho bang `group`
+ALTER TABLE `group`
+	ADD PRIMARY KEY (`group_id`);
+-- Tao key cho bang `student`
+ALTER TABLE `student`
+  ADD PRIMARY KEY (`student_id`,`group_id`),
+  ADD CONSTRAINT FOREIGN KEY (`group_id`) REFERENCES `group`(`group_id`);
+-- Tao key cho bang `meeting`
+ALTER TABLE `meeting`
+	ADD PRIMARY KEY (`meeting_id`,`group_id`,`teacher_id`),
+    ADD CONSTRAINT FOREIGN KEY (`group_id`) REFERENCES `group`(`group_id`),
+    ADD CONSTRAINT FOREIGN KEY (`teacher_id`) REFERENCES `teacher`(`teacher_id`);
