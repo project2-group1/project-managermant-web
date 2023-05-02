ad1fda9db49cb47b336605d276f6a0807cb77ce5
mxngocqb <87193435+mxngocqb@users.noreply.github.com>
2023-05-02 10:49:01 +0700

    Update by Ngoc, make calender

---
+++ frontend/main.js
@@ -0,0 +1,18 @@
+const $ = document.querySelector.bind(document);
+const $$ = document.querySelectorAll.bind(document);
+// XỬ lý add calendar
+const btn_add_calendar = $('.btn-add-calendar');
+const btn_close_calendar = $('.btn-close-calendar');
+const make_calendar_container = $('#make-calendar-container');
+
+console.log(btn_close_calendar);
+
+btn_add_calendar.onclick = function(){
+    make_calendar_container.classList.add('show');
+}
+
+btn_close_calendar.onclick = function(){
+    make_calendar_container.classList.remove('show');
+}
+
+// kết thúc xử lý
