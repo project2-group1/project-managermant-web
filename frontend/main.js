28aaeed07776de4f09c799c890ffbd24cbe36600
mxngocqb <87193435+mxngocqb@users.noreply.github.com>
2023-05-02 15:45:49 +0700

    Update by Ngoc, make calender

--- frontend/main.js
+++ frontend/main.js
@@ -1,18 +1,27 @@
 const $ = document.querySelector.bind(document);
 const $$ = document.querySelectorAll.bind(document);
 // XỬ lý add calendar
-const btn_add_calendar = $('.btn-add-calendar');
-const btn_close_calendar = $('.btn-close-calendar');
+const btn_add_calendar = $('.btn.btn-add-calendar');
+const btn_close_calendar = $('.btn.btn-close-calendar');
 const make_calendar_container = $('.make-calendar-container');
 
-console.log(btn_close_calendar);
-
-btn_add_calendar.onclick = function(){
+btn_add_calendar.onclick = function () {
     make_calendar_container.classList.add('show');
 }
 
-btn_close_calendar.onclick = function(){
+btn_close_calendar.onclick = function () {
     make_calendar_container.classList.remove('show');
 }
-
 // kết thúc xử lý
+
+// hiện ẩn side bar
+const btn_sidebar = $('.btn.btn-sidebar');
+const wrapperSidebar = $('.wrapper-sidebar')
+// wrapperSidebar.style.display = "none"
+btn_sidebar.onclick = function() {
+    if (wrapperSidebar.style.display == "none") {
+        wrapperSidebar.style.display = "block";
+    } else {
+        wrapperSidebar.style.display = "none";
+    }
+};
