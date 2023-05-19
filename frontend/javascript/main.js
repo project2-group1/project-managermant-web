ff0ceec565ca5cf574da17ac88e3f7bf8cf4f832
mxngocqb <87193435+mxngocqb@users.noreply.github.com>
2023-05-19 16:22:46 +0700

    hihi

--- frontend/javascript/main.js
+++ frontend/javascript/main.js
@@ -2,20 +2,53 @@ const $ = document.querySelector.bind(document);
 const $$ = document.querySelectorAll.bind(document);
 // XỬ lý add calendar
 const btn_add_calendar = $('.btn.btn-add-calendar');
-const btn_close_calendar = $('.btn.btn-close-calendar');
 const make_calendar_container = $('.make-calendar.container');
+
 
 if (btn_add_calendar) {
     btn_add_calendar.onclick = function () {
+        var text = `
+            <div class="modal">
+                <div class="modal-header">
+                    <h3 class="title">Thêm cuộc họp mới</h3>
+                    <button onclick="closeCalendar()"class="btn btn-close-calendar"><i class="fa-solid fa-xmark"></i></button>
+                </div>
+                <div class="modal-body">
+                    <form>
+                        <label for="title">Tiêu đề:</label><br>
+                        <input type="text" id="title" name="title" placeholder="Thêm tiêu đề"><br>
+
+                        <label for="start_time">Thời gian bắt đầu:</label><br>
+                        <input type="datetime-local" id="start_time" name="start_time"><br>
+
+                        <label for="group">Nhóm tham gia:</label><br>
+                        <select id="group" name="group">
+                            <option value="group1">Nhóm 1</option>
+                            <option value="group2">Nhóm 2</option>
+                            <option value="group3">Nhóm 3</option>
+                        </select><br>
+                        <label for="deadline">Deadline báo báo:</label><br>
+                        <input type="datetime-local" id="end_time" name="end_time"><br>
+                        <label for="subject">Subject</label>
+                        <textarea id="subject" name="subject" placeholder="Ghi chú" style="height:200px"></textarea>
+                        <input type="submit" value="Thêm cuộc họp mới">
+                        
+                    </form>
+                </div>
+            </div>
+         `
+        make_calendar_container.innerHTML = text;
         make_calendar_container.classList.add('show');
     }
 }
 
-if (btn_close_calendar) {
-    btn_close_calendar.onclick = function () {
+var closeCalendar = function () {
+    const btn_close_calendar = $('.btn.btn-close-calendar');
+    if (btn_close_calendar) {
         make_calendar_container.classList.remove('show');
     }
 }
+
 // kết thúc xử lý add calendar
 
 /* 
