f268e81d86422545a2c23403ee479e79042ab9a2
Giang Trung Nghia <nghia2905.per@gmail.com>
2023-05-31 15:58:18 +0700

    hello

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ const morgan = require('morgan') // log server
 const route = require('./routes/index.js') // import route
 const handlebars = require('express-handlebars')
 const path = require('path') // lib của nodejs để lấy địa chỉ
+const mysql = require('mysql');
 const app = express()
 const port = 3000
 
@@ -10,6 +11,7 @@ app.use(morgan('combined'))
 
 app.use(express.static(path.join(__dirname,'public')))
 
+// Template Engine
 app.engine(
     'hbs',
     handlebars.engine({
@@ -21,6 +23,19 @@ app.engine(
 )
 app.set('view engine','hbs')
 app.set('views', path.join(__dirname, 'resources/views'))
+
+// Connect Database
+const con = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "test"
+});
+
+con.connect(function(err) {
+    if (err) throw err;
+    console.log("Connected!");
+});
 
 route(app)
 
